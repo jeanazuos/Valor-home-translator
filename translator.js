39f290b4e7a64575f8b9191b6405942e79daa651
Jean Santos <jsantos@igspo.ogmaster.local>
2020-07-23 01:10:41 -0300

    translated some childs

--- translator.js
+++ translator.js
@@ -50,3 +50,51 @@ if(document.getElementsByClassName("menu-exposto__itens__item theme-color-primar
 }
 
 
+// Traduz rodapé
+document.getElementsByClassName("footer__sections__section__name")[0].innerText="Valor"
+document.getElementsByClassName("footer__sections__section__name")[1].innerText="Products"
+document.getElementsByClassName("footer__sections__section__name")[2].innerText="Reader Center"
+document.getElementsByClassName("footer__sections__section__name")[3].innerText="Follow us"
+document.getElementsByClassName("footer__sections__section__name")[4].innerText="Subscribe"
+document.getElementsByClassName("footer__sections__section__name")[5].innerText="Advertise"
+document.getElementsByClassName("footer__sections__section__name")[6].innerText="Selling content"
+
+//Traduz os filhos do rodapé
+filhos = document.getElementsByClassName("footer__sections__section__subsection")
+
+for(var i=0; i<filhos.length; i++){
+    if(filhos[i].textContent.indexOf("Termos de Uso") > -1){
+        filhos[i].firstElementChild.innerText="Terms of service"
+    }else if(filhos[i].textContent.indexOf("Política de Privacidade") > -1){
+        filhos[i].firstElementChild.innerText="Privacy Policy"
+    }else if(filhos[i].textContent.indexOf("Princípios Editoriais") > -1){
+        filhos[i].firstElementChild.innerText="Editorial Principles"
+    }else if(filhos[i].textContent.indexOf("Expediente") > -1){
+        filhos[i].firstElementChild.innerText="Executive Team"
+    }else if(filhos[i].textContent.indexOf("Análise Setorial") > -1){
+        filhos[i].firstElementChild.innerText="Sectorial Analysis"
+    }else if(filhos[i].textContent.indexOf("Edição Impressa") > -1){
+        filhos[i].firstElementChild.innerText="Print Edition"
+    }else if(filhos[i].textContent.indexOf("Valor Investe") > -1){
+        filhos[i].firstElementChild.innerText="Valor Investe"
+    }else if(filhos[i].textContent.indexOf("Valor PRO") > -1){
+        filhos[i].firstElementChild.innerText="Valor PRO"
+    }else if(filhos[i].textContent.indexOf("Valor RI") > -1){
+        filhos[i].firstElementChild.innerText="Valor RI"
+    }else if(filhos[i].textContent.indexOf("Valor International") > -1){
+        filhos[i].firstElementChild.innerText="Valor International"
+    }else if(filhos[i].textContent.indexOf("Revistas e Anuários") > -1){
+        filhos[i].firstElementChild.innerText="Magazines and Yearbooks"
+    }else if(filhos[i].textContent.indexOf("Seminários") > -1){
+        filhos[i].firstElementChild.innerText="Workshops"
+    }else if(filhos[i].textContent.indexOf("Editar Perfil ") > -1){
+        filhos[i].firstElementChild.innerText="Edit Profile"
+    }else if(filhos[i].textContent.indexOf("Newsletter") > -1){
+        filhos[i].firstElementChild.innerText="Newsletter"
+    }else if(filhos[i].textContent.indexOf("Fale Conosco ") > -1){
+        filhos[i].firstElementChild.innerText="Contact us"
+    }else if(filhos[i].textContent.indexOf("Portal do Assinante") > -1){
+        filhos[i].firstElementChild.innerText="Subscriber Portal"
+    }
+
+}
